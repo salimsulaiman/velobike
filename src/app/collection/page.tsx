2a0b9de421449d0c6002edbc810f749511a815cf
salimbahresy <pentingn1@gmail.com>
2024-11-06 13:25:25 +0700

    fix: error in collection

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 import AccessoriesCard from "../component/AccessoriesCard";
