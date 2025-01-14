a3d19bcf5ae1effae7b54c21d0e00b0de83aec13
salimbahresy <pentingn1@gmail.com>
2025-01-14 13:41:14 +0700

    feat: add feature to navigate another account setting

---
+++ src/app/component/account/Shipping.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Shipping() {
+  return (
+    <>
+      <div className="w-full px-[21px] py-[40px]">
+        <h2 className="text-2xl font-medium text-big-stone-950 mb-3">Shipping</h2>
+      </div>
+      {/* {toast && (
+        <div className="toast toast-end z-50">
+          <div className="alert alert-success text-white">
+            <span>Success add new address</span>
+          </div>
+        </div>
+      )} */}
+    </>
+  );
+}
+
+export default Shipping;
